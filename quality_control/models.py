34b559328c873963865af4246e9c03deaa17ea90
okoroko <okorokov.alexei2014@gmail.com>
2024-04-07 22:00:18 +0300

    first commit

---
+++ quality_control/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from tasks.models import Project, Task
+
+
+class BugReport(models.Model):
+    STATUS_CHOICES = [
+        ('New', 'Новая'),
+        ('In_progress', 'В работе'),
+        ('Completed', 'Завершена')
+    ]
+    PRIORITY_CHOICES = [('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')]
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    project = models.ForeignKey(
+        Project,
+        related_name='bug_reports',
+        on_delete=models.CASCADE
+    )
+    task = models.ForeignKey(
+        Task,
+        related_name='bug_reports',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    status = models.CharField(
+        max_length=50,
+        choices=STATUS_CHOICES,
+        default='New',
+    )
+    priority = models.CharField(
+        max_length=1,
+        choices=PRIORITY_CHOICES,
+        default='1',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class FeatureRequest(models.Model):
+    STATUS_CHOICES = [
+        ('Review', 'Рассмотрение'),
+        ('Accepted', 'Принято'),
+        ('Declined', 'Отклонено')
+    ]
+    PRIORITY_CHOICES = [('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')]
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    project = models.ForeignKey(
+        Project,
+        related_name='feature_requests',
+        on_delete=models.CASCADE
+    )
+    task = models.ForeignKey(
+        Task,
+        related_name='feature_requests',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    status = models.CharField(
+        max_length=50,
+        choices=STATUS_CHOICES,
+        default='New',
+    )
+    priority = models.CharField(
+        max_length=1,
+        choices=PRIORITY_CHOICES,
+        default='1',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
