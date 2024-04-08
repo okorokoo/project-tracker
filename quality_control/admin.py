31b5c635a4ad7c347233011eac21fc8760ef3193
okoroko <okorokov.alexei2014@gmail.com>
2024-04-08 13:02:02 +0300

    Add: Bug Report, Feature Request admins

--- quality_control/admin.py
+++ quality_control/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import BugReport, FeatureRequest
 
-# Register your models here.
+
+class BugReportInline(admin.TabularInline):
+    model = BugReport
+    extra = 0
+    fields = ('title', 'description', 'status', 'priority', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+    can_delete = True
+    show_change_link = True
+
+
+class FeatureRequestInline(admin.TabularInline):
+    model = FeatureRequest
+    extra = 0
+    fields = ('title', 'description', 'status', 'priority', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
+    can_delete = True
+    show_change_link = True
+
+
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'created_at', 'status', 'priority')
+    list_filter = ('status', 'priority', 'project', 'task')
+    search_fields = ('name', 'description')
+
+    # inlines = [TaskInline]
+
+
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task', 'project', 'created_at', 'status', 'priority')
+    list_filter = ('status', 'priority', 'project', 'task')
+    search_fields = ('name', 'description')
